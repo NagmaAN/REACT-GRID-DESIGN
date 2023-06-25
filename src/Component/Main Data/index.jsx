4a7c2183e30053de8c753f6589c646c12b4a6b12
NagmaAN <107165534+NagmaAN@users.noreply.github.com>
2023-06-25 22:35:58 +0530

    Update index.jsx

--- src/Component/Main Data/index.jsx
+++ src/Component/Main Data/index.jsx
@@ -8,21 +8,21 @@ const MainData=()=>{
             <Main
             img="https://edyoda.s3.amazonaws.com/media/blog-images/7_reasons_to_learn_Python_u3lFION.jpg"
             title="Fresh Graduate or IT professional Looking... "
-            name="Raja Dhoundiyal"
+            name="Nagma"
             date=" 05 Aug 2023"
             description="If you are a fresher graduate - or new to IT- looking the next job, you need to read this. The last couple pf years zekeLabs gave me a unique opportunity to... "
             />
              <Main
             img="https://edyoda.s3.amazonaws.com/media/blog-images/edyoda_WxvDSK1.jpg"
             title="Introducing you all to EdYoda -"
-            name="Raja Dhoundiyal"
+            name="Nagma"
             date=" 05 Aug 2023"
             description="Really ecstatic and immensely proud to introduce you all to our latest attempt of adding value and making an impact to the world we are a part of. "
             />
              <Main
             img="https://edyoda.s3.amazonaws.com/media/blog-images/devops-edYoda-blog-small.jpg"
             title="From identity crisis to the Success Story "
-            name="Raja Dhoundiyal"
+            name="Nagma"
             date=" 05 Aug 2023"
             description="DevOps is a paradigm shift in the way how software products are being built in the modern day Information Technology (IT) organizations. "
             />
